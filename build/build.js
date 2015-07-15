1e183a75e69c9ffbd1d21d501fe6c5d9bbbd5b84
Sherman Chen <sherman.chens@gmail.com>
2015-07-15 18:05:21 +0800

    Build clock app with bower install

--- build/build.js
+++ build/build.js
@@ -4,8 +4,11 @@
 
 var r = require('r-wrapper');
 var utils = require('utils');
+var sh = new utils.Commander('sh');
 
 exports.execute = function(options) {
+  sh.initPath(utils.getEnvPath());
+  sh.run(['-c', 'cd ' + options.APP_DIR + ' && bower install']);
   var stageAppDir = utils.getFile(options.STAGE_APP_DIR);
   var configFile = utils.getFile(options.APP_DIR, 'build',
     'require_config.jslike');
